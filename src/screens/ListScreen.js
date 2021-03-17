265778fa51f95b7e17a8448a8c6805a09f2f7dab
catalin racz <raczcatalin22@yahoo.com>
2021-03-17 15:28:33 +0200

    initializare proiect

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList} from 'react-native';
+
+const ListScreen = () => {
+    const friends = [
+        { name: 'Friend #1', age: 20},
+        { name: 'Friend #2', age: 30},
+        { name: 'Friend #3', age: 32},
+        { name: 'Friend #4', age: 35},
+        { name: 'Friend #5', age: 36},
+        { name: 'Friend #6', age: 38},
+        { name: 'Friend #7', age: 50},
+        { name: 'Friend #8', age: 12},
+        { name: 'Friend #9', age: 13}
+    ];
+    return (
+    <FlatList
+        keyExtractor={(friend) => friend.name}
+        data={friends} 
+        renderItem={({ item }) => {
+            return <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>;
+        }}
+      />
+    );
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    }
+});
+
+export default ListScreen;
