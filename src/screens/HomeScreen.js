48eb97e50b85c773f9f997bb50227813f6ca5696
catalinracz <raczcatalin22@yahoo.com>
2021-03-19 16:19:54 +0200

    no message

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,11 +7,27 @@ const HomeScreen = ({navigation}) => {
     <Text style={styles.text}>Hi there!</Text>
     <Button 
       onPress={() => navigation.navigate('Components')}
-      title="Go to Components Demo">
-    </Button>
+      title="Go to Components Demo"
+    />
     <Button
       title="Go to List Demo"
       onPress={() => navigation.navigate('List')}
+    />
+    <Button
+    title="Go to Image Demo"
+    onPress={() => navigation.navigate('Image')}
+    />
+    <Button
+    title="Go to Counter Demo"
+    onPress={() => navigation.navigate('Counter')}
+    />
+    <Button
+    title="Go to Color Demo"
+    onPress={() => navigation.navigate('Color')}
+    />
+    <Button
+    title="Go to Square Demo"
+    onPress={() => navigation.navigate('Square')}
     />
   </View> 
   ); 
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -12,7 +12,6 @@ const ListScreen = () => {
         { name: 'Friend #7', age: 50},
         { name: 'Friend #8', age: 12},
         { name: 'Friend #9', age: 13}
-        { name: 'Friend #9', age: 13}
     ];
     return (
     <FlatList
