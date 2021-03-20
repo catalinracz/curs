4b854d45c42cd22333260f140110e6cf651952a0
DESKTOP-M3G54LB\catal <raczcatalin22@yahoo.com>
2021-03-20 18:15:50 +0200

    box

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {View, Text, StyleSheet, Image} from 'react-native';
 
 const ImageDetail = ({ imageSource, title, score}) => {
-    return <View>
+    return (
+    
+    <View>
         <Image source={imageSource}/>
         <Text>{title}</Text>
         <Text>Image Score - {score}</Text>
-        </View>
+    </View>
+    );
 };
 
 const styles = StyleSheet.create ({});
